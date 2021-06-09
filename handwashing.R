b1fa0998bc0621f6f4cc21054e556e37eec117ae
whh96 <66960104+whh96@users.noreply.github.com>
2021-06-09 11:49:59 -0500

    Add files via upload

---
+++ handwashing.R
@@ -0,0 +1,35 @@
+yearly <- read.csv(file.choose())
+
+attach(yearly)
+
+head(yearly)
+
+library(tidyverse)
+
+yearly <- yearly %>%
+  
+  mutate(proportion_deaths = deaths/births)
+
+ggplot(data = yearly, aes(year, proportion_deaths, colour= clinic)) + geom_line()
+
+monthly <- read.csv(file.choose())
+
+monthly <- monthly %>%
+  mutate(proportion_deaths= deaths/births)
+
+head(monthly)
+
+ggplot(monthly, aes(date, proportion_deaths, group = 1)) +
+  geom_line() +
+  labs(x= "Year", y = "Proportion Deaths")
+
+handwashing_start = as.Date('1847-06-01')
+
+monthly <- monthly %>%
+  mutate(handwashing_started = date >= handwashing_start)
+
+ggplot(monthly, aes(date, proportion_deaths, color = handwashing_started, group = 1)) +
+  geom_line() +
+  labs(x = "Year", y= "Proportion Deaths")
+
+
